1f48b727ed16b625b1a7ab8eb3d84563c7adf3f0
Rafael White <rwhite@mctekk.com>
2022-03-21 10:24:41 -0400

    refactor: change value type on doc part

--- src/Apps/Apps/Factories/AppsFactory.php
+++ src/Apps/Apps/Factories/AppsFactory.php
@@ -14,7 +14,7 @@ class AppsFactory extends Factory
     /**
     * The name of the factory's corresponding model.
     *
-    * @var string
+    * @var Apps
     */
     protected $model = Apps::class;
 
